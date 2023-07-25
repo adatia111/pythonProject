d2a656ee508b2356fa92590c1e0257454cdc7036
dipak <adatiaz@sky.com>
2023-07-25 21:07:16 +0100

    commits

---
+++ msg.py
@@ -0,0 +1,10 @@
+msg= input("enter any message:")
+i=0
+space=0
+while i<len(msg):
+    if msg[1]==" ":
+        space+=1
+    i+=1
+print("you have entered",space+1,"words")
+
+
---
+++ string.py
@@ -0,0 +1,4 @@
+msg = "shafeeq"
+print(msg)
+print(msg[1:4])
+print(msg[2])     
